035c2bf1853681bcc514b097e8bfc1d8f1199b67
ozerova <ogozer@ya.ru>
2022-10-02 13:58:50 +0700

    Задача 5

--- src/Main.java
+++ src/Main.java
@@ -44,5 +44,32 @@ public class Main {
     else if (deliveryDistance > 60) {
         System.out.println("Потребуется дней: " + deliveryDays * 3);
     }
-}
+
+    // Задача №5
+        int monthNumber = 1;
+    switch (monthNumber) {
+        case 12:
+        case 1:
+        case 2:
+            System.out.println("Зима.");
+            break;
+        case 3:
+        case 4:
+        case 5:
+            System.out.println("Весна.");
+            break;
+        case 6:
+        case 7:
+        case 8:
+            System.out.println("Лето");
+            break;
+        case 9:
+        case 10:
+        case 11:
+            System.out.println("Осень");
+            break;
+        default:
+            System.out.println("Такого месяца не существует");
+    }
+    }
 }
